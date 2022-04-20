7d7307be245ba306c6def680c9c685d999a8f5a6
Devyn Blackstone <devynblackmon@gmail.com>
2022-04-19 22:24:09 -0500

    saving

--- index.js
+++ index.js
@@ -1,53 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const prompt = () => {
-//     return inquirer.prompt([
-//       {
-//         type: 'input',
-//         name: 'name',
-//         message: 'What is your name?',
-//       },
-//       {
-//         type: 'input',
-//         name: 'id',
-//         message: 'What is your id?',
-//       },
-//       {
-//         type: 'input',
-//         name: 'email',
-//         message: 'What is your email?',
-//       },
-//     ]);
-//   };
+const util = require('util');
 
 // const employeeInfo = () => {
 // prompt()
@@ -57,3 +10,75 @@ const fs = require('fs');
 // };
   
 // employeeInfo();
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your id?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?',
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is your office number?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What school did you go to?',
+    },
+  ]);
+};
+
+const generateHTML = (answers) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team Profile</title>
+</head>
+<body>
+  <header>My Team</header>
+  <div class="card" style="width: 18rem;">
+    <div class="card-header">
+        ${answers.name}
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${answers.id}</li>
+        <li class="list-group-item">Email: ${answers.email}</li>
+        <li class="list-group-item">${answers.school}</li>
+    </ul>
+   </div>
+
+</body>
+</html>`;
+
+// Bonus using writeFileAsync as a promise
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+};
+
+init();
