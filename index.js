8f082673dc7edc19989f119761f719ede3569c19
Devyn Blackstone <devynblackmon@gmail.com>
2022-04-19 23:02:27 -0500

    removed html file

--- index.js
+++ index.js
@@ -14,39 +14,35 @@ const util = require('util');
 // create writeFile function using promises instead of a callback function
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => {
+const promptManager = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?',
+      message: 'What is the team managers name?',
     },
     {
       type: 'input',
       name: 'id',
-      message: 'What is your id?',
+      message: 'What is the team managers id?',
     },
     {
       type: 'input',
       name: 'email',
-      message: 'What is your email?',
+      message: 'What is the team managers email?',
     },
     {
       type: 'input',
       name: 'officeNumber',
-      message: 'What is your office number?',
+      message: 'What is team managers office number?',
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+        type: 'list',
+        name: 'title',
+        message: 'What type of team member do you want to add?',
+        choices: ['Intern', "Engineer", "I'm done adding team members"]
     },
-    {
-      type: 'input',
-      name: 'school',
-      message: 'What school did you go to?',
-    },
-  ]);
+  ])
 };
 
 const generateHTML = (answers) =>
@@ -55,6 +51,8 @@ const generateHTML = (answers) =>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
   <title>Team Profile</title>
 </head>
 <body>
@@ -66,17 +64,20 @@ const generateHTML = (answers) =>
     <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${answers.id}</li>
         <li class="list-group-item">Email: ${answers.email}</li>
-        <li class="list-group-item">${answers.school}</li>
+        <li class="list-group-item">${answers.officeNumber}</li>
     </ul>
    </div>
 
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>`;
 
 // Bonus using writeFileAsync as a promise
 const init = () => {
-  promptUser()
-    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+  promptManager()
+    .then((answers) => writeFileAsync('./dist/index.html', generateHTML(answers)))
     .then(() => console.log('Successfully wrote to index.html'))
     .catch((err) => console.error(err));
 };
---
+++ util/getTitle.js
@@ -0,0 +1,11 @@
+function getTitle(title){
+    if(title = "Intern"){
+        getIntern();
+    }
+    else if (title = "Engineer"){
+        getEngineer();
+    }
+    else{
+        console.log('Done adding members to the team');
+    }
+}
